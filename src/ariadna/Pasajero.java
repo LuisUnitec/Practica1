b987a986cc2520457b62dc6fdc552d46037e1de5
ariadna <ariadna.aburto@my.unitec.edu.mx>
2023-09-25 21:44:17 -0600

    cambio practica 1

---
+++ src/ariadna/Pasajero.java
@@ -0,0 +1,13 @@
+package ariadna;
+
+public class Pasajero {
+    public String id;
+    public String nombre;
+    public int  cantViajesRealizados;
+
+    public String toString(){
+        return nombre + ".CANTIDAD DE VIAJES REALIZADOS: " + cantViajesRealizados;
+
+    }
+
+}
