9d45a2828df582dfc5331e56a458d7cfc6e799d3
IanMedinaB <paco-1204ian@hotmail.com>
2023-09-25 21:40:14 -0600

    Agrego clase pasajero y pruebas

---
+++ src/ian/Pasajero.java
@@ -0,0 +1,15 @@
+package ian;
+
+public class Pasajero {
+    public String id;
+    public String nombre;
+    public int cantViajesRealizados;
+
+
+
+    @Override
+    public String toString(){
+
+        return nombre + ": cantidad de viajes realizados " + cantViajesRealizados;
+    }
+}
