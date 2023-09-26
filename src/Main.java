e90e739e0b0d7d99eeefe15c59b3723c43643070
Luis <ddajps.20202@gmail.com>
2023-09-25 19:25:18 -0600

    Clases base

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Calificacion c = new Calificacion();
+        c.concepto = "Amabilidad";
+        c.calificacion = 4.7;
+
+        Calificacion c2 = new Calificacion();
+        c2.concepto = "Limpieza";
+        c2.calificacion = 3.5;
+
+        System.out.println(c);
+        System.out.println(c2);
+
+        Conductor conductor = new Conductor();
+
+    }
+}
