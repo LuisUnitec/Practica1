6095295647f39f9ca8e182d904e6b42ed14393b3
Luis <ddajps.20202@gmail.com>
2024-01-25 17:47:00 -0600

    Código base

---
+++ src/Conductor.java
@@ -0,0 +1,9 @@
+public class Conductor {
+    String noSocio;
+    String nombre;
+    int cantViajesRealizados;
+
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Conductor conductor = new Conductor();
+        System.out.println(conductor);
+    }
+}
